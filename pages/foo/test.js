b8e1ed84ea731b368899adf9504fa063ee9eb914
Erik Stockmeier <erik.d.stock@gmail.com>
2020-02-26 09:24:35 -0500

    wip

---
+++ pages/foo/test.js
@@ -0,0 +1,1 @@
+export default () => <div>This is a test</div>
--- pages/states/nevada.tsx
+++ pages/states/nevada.tsx
@@ -4,9 +4,14 @@ import fetch from "isomorphic-unfetch"
 import { Layout } from "../../components/Layout"
 import { UserContext } from "../../components/Context"
 import { Main } from "../../components/Main"
-import { Styled } from "theme-ui"
 import { massageResult } from "../../util/precinctData"
-// import { PrecinctData } from "../../util/precinctData"
+
+// get the csv from wherever
+const getCsv = async () => {
+  const result = await fetch("https://nevada-cranks.herokuapp.com/results")
+  const csv = await result.text()
+  return csv
+}
 
 interface Props {
   // data: PrecinctData
@@ -14,7 +19,12 @@ interface Props {
   error?: string
 }
 
-const Nevada: NextPage<Props> = ({ data, error }) => {
+const Nevada: NextPage<Props> = props => {
+  console.log(
+    "ND: ",
+    typeof window !== "undefined" && (window as any).__NEXT_DATA__.page
+  )
+  const { data, error } = props
   if (error) return <p>error: {error}</p>
   const [selectedPrecinct, setSelectedPrecinct] = React.useState(null)
   return (
@@ -28,13 +38,15 @@ const Nevada: NextPage<Props> = ({ data, error }) => {
   )
 }
 
-Nevada.getInitialProps = async ({ query: _query }) => {
+// eslint-disable-next-line @typescript-eslint/camelcase
+export const unstable_getStaticProps = async (...args) => {
+  console.log("unstable_getStaticProps", { args })
+  const csv = await getCsv()
   // if (query.state !== "nevada")
   //   return { data: {}, error: "State not available" }
-  const result = await fetch("https://nevada-cranks.herokuapp.com/results")
-  const csv = await result.text()
+  // console.warn("Get Initial Props")
   const data = massageResult(csv)
-  return { data: data as any }
+  return { props: { data: data as any } }
   // return { data: (results as unknown) as any PrecinctData }
 }
 
