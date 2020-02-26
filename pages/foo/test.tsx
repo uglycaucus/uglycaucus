1d09b80a51ff992e6bbd05f5222be67975943ecc
Erik Stockmeier <erik.d.stock@gmail.com>
2020-02-26 09:37:47 -0500

    wip again

--- pages/foo/test.tsx
+++ pages/foo/test.tsx
@@ -1,5 +1,7 @@
 import React from "react"
-export default ({ buildTime }) => <div>Site built at {buildTime.toString}</div>
+export default ({ buildTime }) => (
+  <div>Site built at {buildTime.toString()}</div>
+)
 
 // eslint-disable-next-line @typescript-eslint/camelcase
 export const unstable_getStaticProps = async (...args) => {
